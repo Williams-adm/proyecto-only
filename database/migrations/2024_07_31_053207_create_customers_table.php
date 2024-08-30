9a47de6df7cb89400666962a6389363c631c760e
WilliamsAdm <i2310607@continental.edu.pe>
2024-08-29 20:10:44 -0500

    modificacion

--- database/migrations/2024_07_31_053207_create_customers_table.php
+++ database/migrations/2024_07_31_053207_create_customers_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 65);
-            $table->string('paternal_surname', 25);
-            $table->string('maternal_surname', 25);
+            $table->string('name', 65)->nullable();
+            $table->string('paternal_surname', 25)->nullable();
+            $table->string('maternal_surname', 25)->nullable();
             $table->date('date_of_birth')->nullable();
+            $table->string('business_name', 85)->nullable();
+            $table->string('fiscal_address ')->nullable();
             $table->timestamps();
         });
     }
