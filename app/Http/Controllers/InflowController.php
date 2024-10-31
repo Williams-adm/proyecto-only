d38dc5f20d04052de4c8ecdbcb6e5b57d88618f4
WilliamsAdm <i2310607@continental.edu.pe>
2024-10-30 23:08:43 -0500

    ligero cambio

---
+++ app/Http/Controllers/InflowController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InflowController extends Controller
+{
+    public function index(){
+        
+    }
+    public function store(){
+
+    }
+    public function show(){
+
+    }
+    public function update(){
+
+    }
+    public function delete(){
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\DiscountController;
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\InflowController;
 use App\Http\Controllers\InventoryController;
 use App\Http\Controllers\NoteCustomerController;
 use App\Http\Controllers\NoteEmployeeController;
@@ -19,16 +20,15 @@ use Illuminate\Support\Facades\Route;
 })->middleware('auth:sanctum'); */
 
 Route::group(['prefix' => 'v1'], function(){
-    Route::apiResource('employees', EmployeeController::class);
-    Route::get('employees/{employee}/phones', [PhoneController::class, 'getPhonesByEmployee'])->name('phones.byEmployee');
-    Route::patch('employees/{employee}/user', [UserController::class, 'update'])->name('user.update');
-    Route::prefix('employees')->controller(NoteEmployeeController::class)->group(function (){
-        Route::post('/{employee}/notes', 'storeNoteForEmployee')->name('storeNote.byEmployee');
-        Route::get('/{employee}/notes', 'getNotesByEmployee')->name('notes.byEmployee') ;
-        Route::match(['put', 'patch'], '/{employee}/notes/{note}', 'update')->name('notesEmployee.update');
-        Route::delete('/{employee}/notes/{note}', 'destroy')->name('notesEmployee.destroy');
+    Route::prefix('branches')->controller(BranchController::class)->group(function () {
+        Route::get('/', 'index')->name('branches.index');
+        Route::post('/', 'store')->name('branches.store');
+        Route::match(['put', 'patch'], '/{branch}', 'update')->name('branches.update');
+        Route::delete('/{branch}', 'destroy')->name('branches.destroy');
     });
 
+    Route::apiResource('categories', CategoryController::class);
+    
     Route::get('customers/business', [CustomerController::class, 'indexBusiness'])->name('customerBusiness.index');
     Route::apiResource('customers', CustomerController::class);
     Route::get('customers/{customer}/phones', [PhoneController::class, 'getPhonesByCustomer'])->name('phones.byCustomer');
@@ -38,27 +38,29 @@ Route::group(['prefix' => 'v1'], function(){
         Route::match(['put', 'patch'], '/{customer}/notes/{note}', 'update')->name('notesCustomer.update');
         Route::delete('/{customer}/notes/{note}', 'destroy')->name('notesCustomer.destroy');
     });
-
-    Route::apiResource('categories', CategoryController::class);
-
-    Route::apiResource('suppliers', SupplierController::class);
-
-    Route::apiResource('inventory', InventoryController::class);
-    Route::prefix('inventory')->controller(InventoryController::class)->group(function (){
-        Route::get('/{inventory}/stocks', 'showStockMinMax')->name('StockMinMax.show');
-    });
-
-    Route::prefix('branches')->controller(BranchController::class)->group(function () {
-        Route::get('/', 'index')->name('branches.index');
-        Route::post('/', 'store')->name('branches.store');
-        Route::match(['put', 'patch'], '/{branch}', 'update')->name('branches.update');
-        Route::delete('/{branch}', 'destroy')->name('branches.destroy');
-    });
-
+    
     Route::apiResource('discounts', DiscountController::class);
     Route::prefix('discounts')->controller(DiscountController::class)->group(function () {
         Route::post('/inventory', 'storeDiscountInventary')->name('discountInventary.store');
         Route::delete('/{discount}/inventory/{inventory}', 'destroyDiscountInventary')->name('discountInventary.destroy');
     });
 
+    Route::apiResource('employees', EmployeeController::class);
+    Route::get('employees/{employee}/phones', [PhoneController::class, 'getPhonesByEmployee'])->name('phones.byEmployee');
+    Route::patch('employees/{employee}/user', [UserController::class, 'update'])->name('user.update');
+    Route::prefix('employees')->controller(NoteEmployeeController::class)->group(function (){
+        Route::post('/{employee}/notes', 'storeNoteForEmployee')->name('storeNote.byEmployee');
+        Route::get('/{employee}/notes', 'getNotesByEmployee')->name('notes.byEmployee') ;
+        Route::match(['put', 'patch'], '/{employee}/notes/{note}', 'update')->name('notesEmployee.update');
+        Route::delete('/{employee}/notes/{note}', 'destroy')->name('notesEmployee.destroy');
+    });
+    
+    Route::apiResource('inventory', InventoryController::class);
+    Route::prefix('inventory')->controller(InventoryController::class)->group(function (){
+        Route::get('/{inventory}/stocks', 'showStockMinMax')->name('StockMinMax.show');
+    });
+
+    Route::apiResource('inventory/inflow', InflowController::class);
+    
+    Route::apiResource('suppliers', SupplierController::class);
 });
